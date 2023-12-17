835b56b79500733ff5807cb39612b37de3c711c3
rmeloche <rachel.meloche@gmail.com>
2023-12-17 07:40:55 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import  Chart  from  "./components/chart.svelte";
+  import  {  visits,  pages,  referrers,  os,  browsers,  devices  }  from  "./data.js";
+</script>
+
+<main>
+  <h3>Monthly Visitors</h3>
+  <section>
+  <Chart {...visits}/>
+  </section>
+  <h3>Top pages and Referrers</h3>
+  <div id="pages">
+  <section>
+    <Chart {...pages}/>
+  </section>
+  <section>
+    <Chart {...referrers}/>
+  </section>
+  </div>
+  <h3>OS, Browsers and Devices</h3>
+  <div id="devices">
+  <section>
+    <Chart {...os} showLegends={true}/>
+  </section>
+  <section>
+    <Chart {...browsers} showLegends={true}/>
+  </section>
+  <section>
+    <Chart {...devices} showLegends={true}/>
+  </section>
+  </div>
+</main>
+
